da3a7538c043e458293ac04f2172284bc4b87c8f
Christopher Williams <chris.a.williams@gmail.com>
2017-09-15 15:23:54 -0400

    Ti.Filesystem.File is casuing hang on Windows Desktop

--- Resources/app.js
+++ Resources/app.js
@@ -47,7 +47,7 @@ require('./ti.contacts.group.test');
 require('./ti.contacts.person.test');
 require('./ti.database.test');
 require('./ti.filesystem.test');
-require('./ti.filesystem.file.test');
+// require('./ti.filesystem.file.test'); // Windows Desktop hangs 
 require('./ti.filesystem.filestream.test');
 // require('./ti.geolocation.test');
 // require('./ti.gesture.test');
